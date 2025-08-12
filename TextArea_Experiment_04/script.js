153347eee4e0d8e101d4d96663cb1611e61e1dcc
Ujjansh Sundram <ujjanshsundram2006@gmail.com>
2025-08-12 14:04:25 +0530

    Basic UI
    character count

---
+++ TextArea_Experiment_04/script.js
@@ -0,0 +1,7 @@
+
+const textarea = document.getElementById('textInput');
+const charCount = document.getElementById('charCount');
+
+textarea.addEventListener('input', () => {
+  charCount.textContent = textarea.value.length;
+});
